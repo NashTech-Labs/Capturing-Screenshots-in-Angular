ff6ad48a73efb033db42a3945fa0b6664daef65d
kiran-0407 <kiran.kaur@knoldus.com>
2023-09-11 13:28:13 +0530

    capturing screenshots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'screenshots-in-angular';
+
+  takeScreenshot(){
+    const captureElement: any = document.getElementById('capture');
+    html2canvas(captureElement).then((canvas: any) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p','px','a4');
+      pdf.addImage(imgData,'PNG',10,10,500,0);
+      //the file name you want
+      pdf.save('myFile.pdf')     
+    });
+  }
 }
